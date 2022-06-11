90003dfb75f5133a913c59792a6a8acb2c342fa9
timzo31 <69592997+timzo31@users.noreply.github.com>
2022-06-11 23:26:34 +0800

    Js variables.

---
+++ Variables/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// We use let to declare a variable when we know ahead of time when its value can be changed through the process.
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstname = "Timera";
+console.log(firstname);
+
+firstname = "Gassama";
+console.log(firstname);
+
+// The standard naming of js variables is camelCase.
+let lastName = "Mamadou";
+
+// variable names cannot start with numbers, but with _, letters or $
+let profession = "Teacher";
+let $function = "Recruiter";
+
+// Constant are declared with uppercase naming convention
+const PI = 3.1415;
+console.log(PI);
