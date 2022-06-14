1c1dca68a6f3a391a57629534972c2eded7a3505
timzo31 <69592997+timzo31@users.noreply.github.com>
2022-06-14 10:02:45 +0800

    Coding Challenge #1, Arrays  in Javascript.

--- Arrays/js/script.js
+++ Arrays/js/script.js
@@ -43,3 +43,15 @@ friends.includes(23);
 
 
 ////////////////// CODING CHALLENGE #1 ARRAYS ////////////////////
+
+const bills = [125, 555, 44];
+
+
+const calcTip = (billVal) => billVal >= 50 && billVal<= 300 ? billVal * .15 : billVal * .2;
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills, tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
