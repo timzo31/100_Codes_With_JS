23090b6f290d47f4c6c5025b37082c88724c4cfc
timzo31 <madty31@gmail.com>
2022-06-25 08:47:38 +0800

    Guess My Number: game loogic and CSS styling.

--- DOM/js/guess.js
+++ DOM/js/guess.js
@@ -1,33 +1,61 @@
 "use strict";
 
 const message = document.querySelector(".message");
-const score = document.querySelector(".score");
+let score = document.querySelector(".score");
 const highscore = document.querySelector(".highscore");
 const number = document.querySelector(".number");
 const guess = document.querySelector(".guess");
 const check = document.querySelector(".check");
 const again = document.querySelector(".again");
 
-let randNumber = Math.floor(Math.random() * 20) + 1;
-console.log(randNumber);
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+let score_ = 20; // State variable
+score.textContent = score_;
 
 check.addEventListener("click", function (e) {
   e.preventDefault();
-  if (guess.value == randNumber) {
+
+  // When there is no input
+  if (!guess.value) {
+    message.textContent = "No number!";
+  }
+  // When the secretNumber is equal to the input number
+  else if (guess.value == secretNumber) {
     message.textContent = "Correct Number!";
     document.body.style.backgroundColor = "#3f8";
-    number.textContent = randNumber;
-  } else if (guess.value > randNumber) {
-    message.textContent = "Should be less!";
-  } else {
-    message.textContent = "Should be greater!";
+    number.textContent = secretNumber;
+    number.style.width = "30rem";
+  }
+  // When the input number is too high
+  else if (guess.value > secretNumber) {
+    if (score_ > 1) {
+      message.textContent = "Too high!";
+      score_--;
+      score.textContent = score_;
+    } else {
+      message.textContent = "You lost the game !";
+      score.textContent = 0;
+    }
+  }
+  // When the input number is too low
+  else {
+    if (score_ > 1) {
+      message.textContent = "Too low!";
+      score_--;
+      score.textContent = score_;
+    } else {
+      message.textContent = "You lost the game !";
+      score.textContent = 0;
+    }
   }
 });
 
 again.addEventListener("click", function (e) {
   e.preventDefault();
 
-  randNumber;
+  secretNumber;
   document.body.style.backgroundColor = "#000";
   number.textContent = "?";
   message.textContent = "Start guessing...";
