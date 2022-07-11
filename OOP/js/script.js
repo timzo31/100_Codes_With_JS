9f7dedb85704022facb6149d03c20373d60b816b
timzo31 <madty31@gmail.com>
2022-07-11 19:13:07 +0800

    inheritance between classes using Object.create.

--- OOP/js/script.js
+++ OOP/js/script.js
@@ -83,3 +83,106 @@ console.log(tesla);
 tesla.accelerate();
 tesla.brake();
 tesla.accelerate();
+
+////////////////////////////////////
+////// INHERITANCE IN ES6 //////////
+////////////////////////////////////
+class PersonCl {
+  constructor(fullName, birthyear) {
+    this.fullName = fullName;
+    this.birthyear = birthyear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthyear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}!`);
+  }
+
+  get age() {
+    return 2037 - this.birthyear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) {
+      this._fullName = name;
+    } else {
+      alert(`$name is not a full name!`);
+    }
+  }
+
+  get fullname() {
+    return this._fullname;
+  }
+
+  // static method
+  static hey() {
+    console.log("Hey there");
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullname, birthyear, course) {
+    // Always need to happen first
+    super(fullname, birthyear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthyear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthyear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl("Niouma GASSAMA", 2001, "economy");
+
+martha.introduce();
+martha.calcAge();
+
+////////////////////////////////////
+// INHERITANCE WITH OBJECT.CREATE //
+////////////////////////////////////
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthyear);
+  },
+
+  init(firstname, birthyear) {
+    this.firstName = firstname;
+    this.birthyear = birthyear;
+  },
+};
+
+const timera = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(
+    `I'm ${this.fullName}. ${
+      2037 - this.birthyear
+    } years old, but as a student I feel more like ${
+      2037 - this.birthyear + 10
+    }`
+  );
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jayson", 1990, "Communication");
+console.log(jay);
+jay.introduce();
