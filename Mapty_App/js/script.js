22fc885693cf726c522859ab3f78db7903743e48
timzo31 <madty31@gmail.com>
2022-07-12 22:57:39 +0800

    Mapty App: Rendering Workout input Form

--- Mapty_App/js/script.js
+++ Mapty_App/js/script.js
@@ -24,6 +24,8 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -33,20 +35,68 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      // Handling clicks on map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+        //   console.log(mapEvent);
+        //   const { lat, lng } = mapEvent.latlng;
+        //   L.marker([lat, lng])
+        //     .addTo(map)
+        //     .bindPopup(
+        //       L.popup({
+        //         maxWidth: 250,
+        //         minWidth: 100,
+        //         autoClose: false,
+        //         closeOnClick: false,
+        //         className: "running-popup",
+        //       })
+        //     )
+        //     .setPopupContent()
+        //     .openPopup();
+      });
     },
     function () {
       alert("could not get your position");
     }
   );
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+  // display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent()
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
