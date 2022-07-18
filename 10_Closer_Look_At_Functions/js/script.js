179e150c840dd627b441eca7f37b220edd7a5823
timzo31 <madty31@gmail.com>
2022-07-19 02:32:14 +0800

    Closer look at functions: Closures examples.

--- 10_Closer_Look_At_Functions/js/script.js
+++ 10_Closer_Look_At_Functions/js/script.js
@@ -287,3 +287,49 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+
+///////////////////////////////////////
+///////   MORE CLOSURE EXAMPLES   /////
+// Example 1
+let f;
+
+const g = function () {
+	const a = 23;
+	f = function () {
+		console.log(a * 2);
+	};
+};
+
+const h = function () {
+	const b = 777;
+	f = function () {
+		console.log(b * 2);
+	};
+};
+
+g();
+f();
+console.dir(f);
+
+// re-asigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+	const perGroup = n / 3;
+
+	setTimeout(function () {
+		console.log(`We are now boarding all ${n}`);
+		console.log(`There are 3 groups, each with ${perGroup} passengers`);
+	}, wait * 1000);
+
+	console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+// setTimeout(function () {
+// 	console.log("TIMER");
+// }, 1000);
