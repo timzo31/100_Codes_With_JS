a011de96e4b10cca30345b3fdd6d016e91721c7f
timzo31 <madty31@gmail.com>
2022-07-24 17:02:13 +0800

    section 09: Data Structures, Modern Operators & Strings: Looping Objects: Keys, Values and Entries done.

--- 09_Data_Structures_Operators&strings/js/script.js
+++ 09_Data_Structures_Operators&strings/js/script.js
@@ -11,11 +11,11 @@ const openingHours = {
 		open: 12,
 		close: 22,
 	},
-	[weekdays[5]]: {
+	[weekdays[4]]: {
 		open: 11,
 		close: 23,
 	},
-	sat: {
+	[weekdays[5]]: {
 		open: 0, // Open 24 hours
 		close: 24,
 	},
@@ -67,8 +67,37 @@ const restaurant = {
 // });
 
 ////////////////////////////////////////////////////////
+/// LOOPING OBJECTS: Object Keys, Values and Entries ///
+////////////////////////////////////////////////////////
+/**/
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+	openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Property ENTRIES
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+	console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+////////////////////////////////////////////////////////
 ////////////      OPTIONAL CHAINING (?.)    ////////////
 ////////////////////////////////////////////////////////
+/*
 if (restaurant.openingHours && restaurant.openingHours.mon)
 	console.log(restaurant.openingHours.mon.open);
 
@@ -96,6 +125,7 @@ const users = [
 ];
 
 console.log(users[0]?.name ?? "User array empty");
+*/
 ////////////////////////////////////////////////////////
 /////////// LOOPING ARRAYS:The For-Of Loop   ///////////
 ////////////////////////////////////////////////////////
