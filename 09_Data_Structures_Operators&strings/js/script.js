426a74b02c943d31d2f38d0c69d6c9d7ab2191bf
timzo31 <madty31@gmail.com>
2022-07-24 16:16:29 +0800

    section 09: Data Structures, Modern Operators & Strings: Enhanced object literals done.

--- 09_Data_Structures_Operators&strings/js/script.js
+++ 09_Data_Structures_Operators&strings/js/script.js
@@ -4,6 +4,23 @@
 const flights =
 	"_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHours = {
+	[weekdays[3]]: {
+		open: 12,
+		close: 22,
+	},
+	fri: {
+		open: 11,
+		close: 23,
+	},
+	sat: {
+		open: 0, // Open 24 hours
+		close: 24,
+	},
+};
+
 // Data needed for first part of the section
 const restaurant = {
 	name: "Classico Italiano",
@@ -11,38 +28,21 @@ const restaurant = {
 	categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
 	starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
 	mainMenu: ["Pizza", "Pasta", "Risotto"],
+	//openingHours: openingHours,
+	// Enhanced ES6 object literals
+	openingHours,
 
-	openingHours: {
-		thu: {
-			open: 12,
-			close: 22,
-		},
-		fri: {
-			open: 11,
-			close: 23,
-		},
-		sat: {
-			open: 0, // Open 24 hours
-			close: 24,
-		},
-	},
-
-	order: function (starterIndex, mainIndex) {
+	order(starterIndex, mainIndex) {
 		return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
 	},
 
-	orderDelivery: function ({
-		starterIndex = 1,
-		mainIndex = 0,
-		time = "20:00",
-		address,
-	}) {
+	orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
 		console.log(
 			`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
 		);
 	},
 
-	orderPasta: function (ing1, ing2, ing3) {
+	orderPasta(ing1, ing2, ing3) {
 		console.log(
 			`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
 		);
@@ -67,9 +67,13 @@ restaurant.orderDelivery({
 });
 
 ////////////////////////////////////////////////////////
-/////////// LOOPING ARRAYS:The For-Of Loop   ///////////
+////////////   ENHANCED OBJECT LITTERALS    ////////////
 ////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////
+/////////// LOOPING ARRAYS:The For-Of Loop   ///////////
+////////////////////////////////////////////////////////
+/*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) {
@@ -81,7 +85,7 @@ for (const [i, el] of menu.entries()) {
 }
 
 //console.log([...menu.entries()]);
-
+*/
 ////////////////////////////////////////////////////////
 ///////////       CODING CHALLENGE N°1       ///////////
 ////////////////////////////////////////////////////////
