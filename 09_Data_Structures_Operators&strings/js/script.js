720520d80a5944d917d3d79183cc79e99e24b75c
timzo31 <madty31@gmail.com>
2022-07-24 15:04:13 +0800

    section 09: Data Structures, Modern Operators & Strings: Logical Assignment Operators accomplished.

--- 09_Data_Structures_Operators&strings/js/script.js
+++ 09_Data_Structures_Operators&strings/js/script.js
@@ -67,9 +67,44 @@ restaurant.orderDelivery({
 });
 
 ////////////////////////////////////////////////////////
-///////////  THE NULLISH COALESCING OPERATORS  /////////
+///////////    LOGICAL ASSIGNMENT OPERATORS  ///////////
 ////////////////////////////////////////////////////////
 
+const rest1 = {
+	name: "Capri",
+	numGuests: 20,
+	numGuests: 0,
+};
+
+const rest2 = {
+	name: "Capri",
+	owner: "Giovani Travis",
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && "<Anonymous>";
+// rest2.owner = rest2.owner && "<Anonymous>";
+
+rest1.owner &&= "<Anonymous>";
+rest2.owner &&= "<Anonymous>";
+
+console.log(rest1);
+console.log(rest2);
+
+////////////////////////////////////////////////////////
+///////////  THE NU LLISH COALESCING OPERATORS  /////////
+////////////////////////////////////////////////////////
+/*
 restaurant.numGuests = 0;
 
 const guests = restaurant.numGuests || 10;
@@ -78,7 +113,7 @@ console.log(guests);
 // Nullish: null and undefined (NOT 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
-
+*/
 ////////////////////////////////////////////////////////
 ////////////   SHORT CIRCUITING (&& AND ||)  ///////////
 ////////////////////////////////////////////////////////
