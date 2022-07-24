895e0623f465598a47c36816645401c4075dc16e
timzo31 <madty31@gmail.com>
2022-07-24 15:54:16 +0800

    section 09: Data Structures, Modern Operators & Strings: Coding Challenge n°1 accomplished.

--- 09_Data_Structures_Operators&strings/js/script.js
+++ 09_Data_Structures_Operators&strings/js/script.js
@@ -67,6 +67,85 @@ restaurant.orderDelivery({
 });
 
 ////////////////////////////////////////////////////////
+///////////       CODING CHALLENGE N°1       ///////////
+////////////////////////////////////////////////////////
+
+const game = {
+	team1: "Barcelona",
+	team2: "Real Madrid",
+	players: [
+		[
+			"Stergen",
+			"Pique",
+			"christensen",
+			"Alba",
+			"Araujo",
+			"Busquets",
+			"Pedri",
+			"Gavi",
+			"Rafinha",
+			"Fati",
+			"Lewandowski",
+		],
+		[
+			"Courtois",
+			"Militao",
+			"Alaba",
+			"Mendy",
+			"Vasquez",
+			"Kroos",
+			"Modric",
+			"Valverde",
+			"Rodrigo",
+			"Camavinga",
+			"Benzema",
+		],
+	],
+	score: "4:0",
+	scored: ["Lewandoski", "Fati", "Rafinha", "Lewandowski"],
+	date: "Jul 24th, 2022",
+	odds: {
+		team1: 6.5,
+		x: 3.25,
+		team2: 1.33,
+	},
+};
+
+// 1)
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2)
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4)
+const players1Final = [...players1, "Dembele", "Feran", "Kessie"];
+
+// 5)
+const {
+	odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6)
+const printGoals = function (...players) {
+	console.log(`${players.length} goals scored`);
+};
+
+// printGoals("Lewandowski", "Fati", "Rafinha", "Lewandowski");
+// printGoals("Benzema", "Modric", "Aubameyang", "Pedri", "Busquets");
+printGoals(...game.scored);
+
+// 7)
+team1 < team2 && console.log("Team 2 is more likely to win");
+
+team1 > team2 && console.log("Team 1 is more likely to win");
+////////////////////////////////////////////////////////
 ///////////    LOGICAL ASSIGNMENT OPERATORS  ///////////
 ////////////////////////////////////////////////////////
 
