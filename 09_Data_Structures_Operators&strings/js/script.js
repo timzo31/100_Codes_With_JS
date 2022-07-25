5c8afe3dcf7ebab440833227fdd0c8d00cd16d9a
timzo31 <madty31@gmail.com>
2022-07-26 01:21:42 +0800

    section 09: Data Structures, Modern Operators & Strings: Looping Objects: Map Iteration done.

--- 09_Data_Structures_Operators&strings/js/script.js
+++ 09_Data_Structures_Operators&strings/js/script.js
@@ -67,9 +67,9 @@ const restaurant = {
 // });
 
 ////////////////////////////////////////////////////////
-/////                    MAPS                      /////
+/////             MAPS FUNDAMENTALS                /////
 ////////////////////////////////////////////////////////
-
+/*
 const rest = new Map();
 rest.set("name", "El Classico");
 rest.set(1, "Firenzo, Italy");
@@ -101,6 +101,46 @@ console.log(rest);
 console.log(rest.size);
 
 console.log(rest.get(arr, "Test"));
+*/
+
+////////////////////////////////////////////////////////
+/////                MAPS ITERATION                /////
+////////////////////////////////////////////////////////
+
+const question = new Map([
+	["question", "What is the best programming language in the world?"],
+	[1, "C"],
+	[2, "Java"],
+	[3, "JavaScript"],
+	["correct", 3],
+	[true, "Correct :)"],
+	[false, "Try again!"],
+]);
+console.log(question);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+	if (typeof key === "number") console.log(`answer ${key}:  ${value}`);
+}
+
+// const answer = Number(prompt("Your answer"));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get("correct") === answer));
+
+// Convert Map to Array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
 
 ////////////////////////////////////////////////////////
 /////                    SETS                      /////
