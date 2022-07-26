5bfdc31b3dcdd99949c8218e7bdea803e2272bfe
timzo31 <madty31@gmail.com>
2022-07-26 17:05:15 +0800

    section 09: Data structures, Modern Operators and Strings: Working with Strings: String Methods Practice done successfully.

--- 09_Data_Structures_Operators&strings/js/script.js
+++ 09_Data_Structures_Operators&strings/js/script.js
@@ -4,6 +4,26 @@
 const flights =
 	"_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
+// . Delayed Departure from FAO to TXL (11h25)
+// ............Arrival from BRU to FAO (11h45)
+// ... Delayed Arrival from HEL to FAO (12h05)
+// ......... Departure from FAO to LIS (12h30)
+
+//console.log(flights.split("+"));
+
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split("+")) {
+	//console.log(flight);
+	const [type, from, to, time] = flight.split(";");
+	const output = `${
+		type.startsWith("_Delayed") ? "Emoji" : " "
+	}${type.replaceAll("_", " ")} from ${getCode(from)} to ${getCode(
+		to
+	)} (${time.replace(":", "h")})`.padStart(48);
+	console.log(output);
+}
+
 const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 const openingHours = {
@@ -526,15 +546,8 @@ document.querySelector("button").addEventListener("click", function () {
 			second[0].toUpperCase()
 		)}`;
 		console.log(`${output.padEnd(20)}${"checked".repeat(i + 1)}`);
-
-		// camelSplit = [camelFirstWorld, camelSecondWorld];
-		// console.log(camelSplit);
-		// 	.join("", n[0].toLowerCase() + n[1].toUpperCase());
-		// console.log(camelSplit);
 	}
 });
-
-const camelDisplay = function (words) {};
 
 // camelDisplay(text);
 
