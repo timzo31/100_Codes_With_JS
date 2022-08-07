64f2309e7c0c88f2327e8c61354a1bdfad0b5eb5
timzo31 <madty31@gmail.com>
2022-08-07 16:22:08 +0800

    Section 11: Challenge N°4, Done successfully.

--- 11_Working_With_Arrays/Challenges/js/script.js
+++ 11_Working_With_Arrays/Challenges/js/script.js
@@ -36,7 +36,7 @@ checkDogs(juliaData, kateData);
 ///////////      CHALLENGE N°2        ////////////////
 //////////////////////////////////////////////////////
 
-const dogs = [5, 2, 4, 1, 15, 8, 3];
+const dogs1 = [5, 2, 4, 1, 15, 8, 3];
 const dogs2 = [16, 6, 10, 5, 6, 1, 4];
 
 const calcAverageHumanAge = function (ages) {
@@ -54,7 +54,7 @@ const calcAverageHumanAge = function (ages) {
   return average;
 };
 
-const avg1 = calcAverageHumanAge(dogs);
+const avg1 = calcAverageHumanAge(dogs1);
 const avg2 = calcAverageHumanAge(dogs2);
 console.log(avg1, avg2);
 
@@ -68,9 +68,70 @@ const calcAverageHumanAgeChain = ages =>
     .filter(age => age >= 18)
     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-const avgChain1 = calcAverageHumanAgeChain(dogs);
+const avgChain1 = calcAverageHumanAgeChain(dogs1);
 console.log(avgChain1);
 
 /////////////////////////////////////////////
 ///////////    CHALLENGE N°4   //////////////
 ////////////////////////////////////////////
+
+// TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
+
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'Much' : 'Little'
+  }`
+);
+
+// 3.
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooLittle);
+
+// 4.
+//"Matilda and Alice and Bob's dogs eat too much!";
+//"Sarah and John and Michael's dogs eat too little!";
+
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5.
+console.log(dogs.some(dog => dog.cur === dog.recFood));
+
+// 6.
+// current > (recommandedFood * .9) && current < (recommanded * 1.1)
+
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+// 6.
+console.log(dogs.filter(checkEatingOkay));
+
+// 8. Sort it by recommanded food portion in an ascending order
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
