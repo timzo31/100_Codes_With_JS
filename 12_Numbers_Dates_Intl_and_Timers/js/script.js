a0556c55df18859c8184f0981d339d6427b2f7f2
timzo31 <madty31@gmail.com>
2022-08-08 14:30:12 +0800

    Section 12: Numeric Separators, Done.

--- 12_Numbers_Dates_Intl_and_Timers/js/script.js
+++ 12_Numbers_Dates_Intl_and_Timers/js/script.js
@@ -357,3 +357,24 @@ labelBalance.addEventListener('click', function () {
     }
   });
 });
+
+////////////////////////////////////
+//////   NUMERIC SEPARATOR     /////
+
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+console.log(Number('230000'));
+console.log(parseInt('230000'));
+
+////////////////////////////////////
+//////   Working with BIGINT   /////
