97c171b836a095b8f1841758888ee21de641a23b
timzo31 <madty31@gmail.com>
2022-08-08 14:22:09 +0800

    Section 12: The remainder Operator, Done.

--- 12_Numbers_Dates_Intl_and_Timers/js/script.js
+++ 12_Numbers_Dates_Intl_and_Timers/js/script.js
@@ -330,3 +330,30 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log((+2.345).toFixed(2));
+
+////////////////////////////////////
+////// THE REMAINDER OPERATOR  /////
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+// stripped rows
+// Every Nth time, use remainder
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'orangered';
+    }
+    if (i % 3 === 0) {
+      row.style.backgroundColor = 'blue';
+    }
+  });
+});
