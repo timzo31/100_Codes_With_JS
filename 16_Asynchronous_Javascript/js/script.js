0c38d08343201d376244390347785966417b4a5b
timzo31 <madty31@gmail.com>
2022-08-10 18:38:16 +0800

    Section 16: Asynchronous Javascript, Chaining Promises, Done.

--- 16_Asynchronous_Javascript/js/script.js
+++ 16_Asynchronous_Javascript/js/script.js
@@ -8,7 +8,7 @@ const countriesContainer = document.querySelector('.countries');
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
-    <img class="country__img" src="${data.flags.svg}" />
+    <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
@@ -83,10 +83,24 @@ setTimeout(() => {
 
 // const request = fetch(`https://restcountries.com/v3.1/name/senegal`);
 // console.log(request);
+// fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
 
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
-getCountryData('portugal');
+
+getCountryData('france');
