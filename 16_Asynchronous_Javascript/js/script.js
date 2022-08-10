94de745fd1c9b3f5ca8c3654e28821322541162c
timzo31 <madty31@gmail.com>
2022-08-10 18:09:03 +0800

    Section 16: Promises & Fetch API, Done.

--- 16_Asynchronous_Javascript/js/script.js
+++ 16_Asynchronous_Javascript/js/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-
+/*
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
@@ -75,3 +75,11 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+*/
+
+// const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+const request = fetch(`https://restcountries.com/v3.1/name/senegal`);
+console.log(request);
