c0b424f270a0211454779b5f772622526b5d7709
timzo31 <madty31@gmail.com>
2022-08-13 04:33:54 +0800

    Section 16: Promises Coding Challenge n°2, To FIX

--- 16_Asynchronous_Javascript/Challenges/js/script.js
+++ 16_Asynchronous_Javascript/Challenges/js/script.js
@@ -75,4 +75,49 @@ const whereAmI = function (lat, lng) {
 
 whereAmI(13.102, -12.308);
 */
+
 ///////////////////////////////////
+//////// CHALLENGE N°2   //////////
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout((resolve, seconds * 1000));
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(resolve => {
+    const img = document.querySelector('.images');
+    img.src = imgPath;
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Img 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
